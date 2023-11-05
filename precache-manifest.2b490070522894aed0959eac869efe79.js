0a9c896313157571de7a084446973f92b4ac3e7c
areeba-junaid <79707470+areeba-junaid@users.noreply.github.com>
2023-11-06 01:01:12 +0500

    Updates

---
+++ precache-manifest.2b490070522894aed0959eac869efe79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3910724f741c39b786901040b72724b6",
+    "url": "/expense-tracker-app/index.html"
+  },
+  {
+    "revision": "dd999d8eba60c0f4b890",
+    "url": "/expense-tracker-app/static/css/main.c307c30d.chunk.css"
+  },
+  {
+    "revision": "db3e77d9e3941a16ca30",
+    "url": "/expense-tracker-app/static/js/2.71737c9c.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/expense-tracker-app/static/js/2.71737c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd999d8eba60c0f4b890",
+    "url": "/expense-tracker-app/static/js/main.9bbc789b.chunk.js"
+  },
+  {
+    "revision": "50172657913c4273b109",
+    "url": "/expense-tracker-app/static/js/runtime-main.510a04ce.js"
+  }
+]);
